48e810d52c1611e382384fda79ea3bcb86d5d969
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 20:29:28 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests as req
+import json
+
+def gateway(request):
+    #set default response
+    response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Internal Error']}}]})
+    
+    #get request payload
+    json_parse = request.get_json() 
+
+    #Get Parameter
+    param = json_parse.get('queryResult', {}).get('intent', {}).get('displayName')
+
+    #Route Case
+
+    return json.dumps({'fulfillmentMessages':[{'text':{'text':[param]}}]})
