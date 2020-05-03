36f6213eaee514e161e9cc59f9ad977f71ee8e0b
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-03 11:01:52 +0700

    debugging

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import json
 
 def gateway(request):
     #set default response
-    response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Mashoookkk']}}]})
+    response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Gateway Error']}}]})
     
     #get request payload
-    #json_parse = request.get_json() 
+    json_parse = request.get_json() 
 
     #Get Parameter
-    #param = json_parse.get('queryResult', {}).get('intent', {}).get('displayName')
+    param = json_parse.get('queryResult', {}).get('intent', {}).get('displayName')
 
     #Route Case
-
-    #return json.dumps({'fulfillmentMessages':[{'text':{'text':[param]}}]})
+    if param == 'IPSearchEngine':
+        #ke routing
+        response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Mencari IP']}}]})
+    elif param == 'ErrorList':
+        #ke routing
+        response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Mencari Error']}}]})
+    else:
+        response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Punten, Mamang teu paham yeuh....']}}]})
+    
     return response
